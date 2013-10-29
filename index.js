41b40ef031ca1b638617ce17979eace6ddf2fca0
py8765 <pengyang633@126.com>
2013-10-29 15:04:37 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var fs = require('fs');
-
-exports.modules = {};
-fs.readdirSync(__dirname + '/lib/modules').forEach(function(filename) {
-	if (/\.js$/.test(filename)) {
-		var name = filename.substr(0, filename.lastIndexOf('.'));
-		var _module = require('./lib/modules/' + name);
-		if (!_module.moduleError) {
-			exports.modules.__defineGetter__(name, function() {
-				return _module;
-			});
-		}
-	}
-});
+module.exports = require('./lib/modules/rpcDebug');
